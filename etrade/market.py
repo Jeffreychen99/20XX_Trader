68520d2ebd23bf2c939811567d5f03618bae0f07
Jeffreychen99 <jeffrey_chen99@berkeley.edu>
2021-02-26 12:22:21 -0800

    Moved logging to logs dir

--- etrade/market.py
+++ etrade/market.py
@@ -5,7 +5,7 @@ from logging.handlers import RotatingFileHandler
 # logger settings
 logger = logging.getLogger('my_logger')
 logger.setLevel(logging.DEBUG)
-handler = RotatingFileHandler("python_client.log", maxBytes=5 * 1024 * 1024, backupCount=3)
+handler = RotatingFileHandler("logs/etrader.log", maxBytes=5 * 1024 * 1024, backupCount=3)
 FORMAT = "%(asctime)-15s %(message)s"
 fmt = logging.Formatter(FORMAT, datefmt='%m/%d/%Y %I:%M:%S %p')
 handler.setFormatter(fmt)
