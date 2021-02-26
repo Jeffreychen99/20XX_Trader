0549a69179161472805eaecfad9ce3ca0a888281
Jeffreychen99 <jeffrey_chen99@berkeley.edu>
2021-02-25 21:05:04 -0800

    Moved etrade logging to etrade dir

--- etrade/auth.py
+++ etrade/auth.py
@@ -16,7 +16,7 @@ config.read('etrade/config.ini')
 # logger settings
 logger = logging.getLogger('my_logger')
 logger.setLevel(logging.DEBUG)
-handler = RotatingFileHandler("python_client.log", maxBytes=5*1024*1024, backupCount=3)
+handler = RotatingFileHandler("etrade/etrader.log", maxBytes=5*1024*1024, backupCount=3)
 FORMAT = "%(asctime)-15s %(message)s"
 fmt = logging.Formatter(FORMAT, datefmt='%m/%d/%Y %I:%M:%S %p')
 handler.setFormatter(fmt)
